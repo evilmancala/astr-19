1bd9ff40dbcbac4b98a11f5116d28390836bd3c0
evilmancala <ner0tic77@gmail.com>
2025-04-17 23:58:29 -0700

    Added Session 1 Prompt
    Just going with the flow.

---
+++ session1_prompt.py
@@ -0,0 +1,22 @@
+#This program will write
+# My name, pronouns, and two sentences about my favorite movie and food.
+
+#This function tells the 
+#computer to print
+#What I listed above.
+
+#I put these all on different lines so it wouldn't be chunky. Hopefully I didn't mess up. LOL!!
+
+def PrintName():
+	print("Hello! My name is Kirin Westerlund.")
+	print("I use they/them pronouns.")
+	print("My favorite movie right now is Ponyo.")
+	print("And my favorite food is tonkatsu (with rice!).")
+
+def main():
+	PrintName()
+
+#When we run the program
+#this executes first
+if __name__ == '__main__':
+	main()

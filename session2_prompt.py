e2ea027cf6da59bc9621771877e1c24d0a94a128
evilmancala <ner0tic77@gmail.com>
2025-04-18 00:37:18 -0700

    Added  session prompt 2
    Another one!

---
+++ session2_prompt.py
@@ -0,0 +1,58 @@
+#Write python: Sum of 2 floating point numbers
+
+#---------------------------------
+#Arithemetic addition
+
+
+x = 2.006 		#first floating point
+y = 70.706		#second floating point
+
+print("x+y = ",x+y) 
+
+#----
+
+#Assignment addition
+
+x = 2.006		#same first and second floating points
+x += 70.706 
+print("x = 2.006; x+=70.706; x = ",x) 	
+
+#---------------------------------
+
+#Write difference of two integers
+
+#---------------------------------
+#Arithemetic subtraction
+
+a = 999 #first integer
+b = 25 	#second integer
+
+print("a-b = ",a-b) 
+
+#----
+#Assignment subtraction
+
+a = 999
+a -= 25  
+print("a = 999; b-=25; a = ",a)
+
+#---------------------------------
+
+#Write product of a floating point number and an integer
+
+#---------------------------------
+#Arithemetic multiplication
+
+fp = 12.31
+i = 18
+
+print("fp*i = ",fp*i) #multiplication
+
+#----
+#Assignment multiplication
+
+fp *= 18  
+print("fp *= 18; fp = ",fp)
+
+
+
